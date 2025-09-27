77b36a4a15d33982619eb16ec203e48faae97486
danialmkj <www.danimalekdev@gmail.com>
2025-09-27 17:31:18 +0330

    feat: connect TaskListScreen into MainActivity

--- app/src/main/java/com/example/danial_maleki_appdevtask/MainActivity.kt
+++ app/src/main/java/com/example/danial_maleki_appdevtask/MainActivity.kt
@@ -3,45 +3,18 @@ package com.example.danial_maleki_appdevtask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.danial_maleki_appdevtask.ui.TaskListScreen.TaskListScreen
 import com.example.danial_maleki_appdevtask.ui.theme.Danial_Maleki_AppDevTaskTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Danial_Maleki_AppDevTaskTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TaskListScreen(modifier = Modifier.fillMaxSize())
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Danial_Maleki_AppDevTaskTheme {
-        Greeting("Android")
     }
 }
